7730c290a477e08e7d9f1871c4e46896194242c5
Deonaf24 <deonaftahi@gmail.com>
2025-09-08 14:26:43 -0400

    added logo

--- src/popup.js
+++ src/popup.js
@@ -15,7 +15,7 @@ const show = (el) => el && el.classList.remove("hidden");
 const hide = (el) => el && el.classList.add("hidden");
 
 const logoImg = $("#logo");
-if (logoImg) logoImg.src = chrome.runtime.getURL("icons/icon.svg");
+if (logoImg) logoImg.src = chrome.runtime.getURL("icons/icon-128.png");
 
 // Basic HTML escaping to prevent injection when building strings
 function escapeHtml(str = "") {
